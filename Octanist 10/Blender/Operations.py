0e6fe610aa3fb2fdff95ad6bd93f1d8f9e08390c
Landon-Dou <landon.dou@outlook.com>
2019-05-03 04:21:32 -0400

    Updated operations in case the communications under socket works - Added communications.py

--- Octanist 10/Blender/Operations.py
+++ Octanist 10/Blender/Operations.py
@@ -2,14 +2,18 @@ import os
 import bpy
 from bpy.app.handlers import persistent
 from datetime import datetime
+import communications
 
 TIMER = None
 
-# Events
+'''
+Events Handlers
+'''
 def clear_render_event():
     bpy.app.handlers.render_init.clear()
     bpy.app.handlers.render_post.clear()
     bpy.app.handlers.render_complete.clear()
+    bpy.app.handlers.render_cancel.clear()
 
 def handler_init_render(scene):
     # Set timer
@@ -17,67 +21,80 @@ def handler_init_render(scene):
     TIMER = datetime.now()
     # Get dir of render ouput
     path = bpy.context.scene.render.filepath
-    if(path=='/tmp\\'): 
+    if(path == '/tmp\\'):
         path = 'C:\\tmp'
     # Send response via socket
     res = {
-        'type': 'msg',
-        'code': 'init_render',
-        'info': {
-            'output_path': path
+        "type": "msg",
+        "code": "init_render",
+        "info": {
+            "frame_total": bpy.context.scene.frame_end - bpy.context.scene.frame_start + 1,
+            "output_path": path
         }
     }
-    sock.send(res)
+    communications.send(res)
 
 def handler_post_render(scene):
     res = {
-        'type': 'msg',
-        'code': 'post_render',
-        'info': {
-            'time_elapsed': datetime.now() - TIMER
+        "type": "msg",
+        "code": "post_render",
+        "info": {
+            "frame_current": bpy.context.scene.frame_current,
+            "time_elapsed": str(datetime.now() - TIMER)
         }
     }
-    sock.send(res)
+    communications.send(res)
 
 def handler_complete_render(scene):
     res = {
-        'type': 'msg',
-        'code': 'complete_render',
-        'info': {
-            'time_elapsed': datetime.now() - TIMER
+        "type": "msg",
+        "code": "complete_render",
+        "info": {
+            "time_elapsed": str(datetime.now() - TIMER)
         }
     }
-    sock.send(res)
+    communications.send(res)
 
-# Operations
+def handler_cancel_render(scene):
+    res = {
+        "type": "msg",
+        "code": "cancel_render",
+        "info": {
+            "time_elapsed": str(datetime.now() - TIMER)
+        }
+    }
+    communications.send(res)
+
+bpy.app.handlers.render_init.append(handler_init_render)
+bpy.app.handlers.render_post.append(handler_post_render)
+bpy.app.handlers.render_complete.append(handler_complete_render)
+bpy.app.handlers.render_cancel.append(handler_cancel_render)
+
+
+'''
+Operations
+'''
 def launch():
     '''
     Launch the Octanist App located in the current directory
     '''
     octanist = os.path.dirname(os.path.realpath(__file__)) + 'Octanist.exe'
     os.startfile(octanist)
+    communications.connect()
 
 def start_render(animation=False):
     '''
     Start rendering the current scene
-    
+
     Parameters
     ----------
     animation : bool
         Render animation or not
     '''
-    # Clean events
-    clear_render_event()
-
     if(animation):
         bpy.ops.render.render(animation=True, write_still=True)
     else:
         bpy.ops.render.render(write_still=True)
-    
-    # Register events
-    bpy.app.handlers.render_init.append(handler_init_render)
-    bpy.app.handlers.render_post.append(handler_post_render)
-    bpy.app.handlers.render_complete.append(handler_complete_render)
 
 def send_selected_info():
     '''
@@ -101,7 +118,7 @@ def append_material_from_lib(libpath, matname):
         The path to the library
     matname : str
         The specific material name
-    
+
     Comments
     --------
     If we copy a material in the library of Octanist App and paste it
---
+++ Octanist 10/Blender/communications.py
@@ -0,0 +1,88 @@
+import time
+import threading
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5200
+BUFFER_SIZE = 1024
+SLEEP_RATE = 0.1
+SOCK = None
+SUCCESS_RES = {
+    "type": "sys",
+    "code": "client_connected"
+}
+
+# Socket Client
+
+
+def get_new_socket():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    return sock
+
+
+def socket_update():
+    '''
+    Socket listener running in thread
+    '''
+    global SOCK
+    # First time connection
+    try:
+        sock = get_new_socket()
+        sock.connect((TCP_IP, TCP_PORT))
+        connected = True
+        print('# Connected to Octanist')
+        SOCK = sock
+        send(SUCCESS_RES)
+    except socket.error:
+        connected = False
+        SOCK = None
+        print('! Waiting for socket connection')
+        while not connected:
+            try:
+                sock = get_new_socket()
+                sock.connect((TCP_IP, TCP_PORT))
+                connected = True
+                print('# Connected to Octanist')
+                SOCK = sock
+                send(SUCCESS_RES)
+            except socket.error:
+                time.sleep(SLEEP_RATE)
+
+    # Disconnection detection and awaiting for connection
+    while(True):
+        data = sock.recv(BUFFER_SIZE).decode('utf-8').strip()
+        if not data:
+            connected = False
+            SOCK = None
+            print('! Waiting for socket connection')
+            while not connected:
+                try:
+                    sock = get_new_socket()
+                    sock.connect((TCP_IP, TCP_PORT))
+                    connected = True
+                    print('# Connected to Octanist')
+                    SOCK = sock
+                    send(SUCCESS_RES)
+                except socket.error:
+                    time.sleep(SLEEP_RATE)
+        else:
+            print('# Socket Received Data')
+            print('- Content: \n', data)
+            time.sleep(SLEEP_RATE)
+
+
+def send(res=None):
+    while(SOCK == None):
+        time.sleep(SLEEP_RATE)
+    if(res != None):
+        SOCK.send(bytes(str(res), 'utf-8'))
+
+
+def connect():
+    thread_socket = threading.Thread(target=socket_update)
+    thread_send = threading.Thread(target=send)
+    thread_socket.start()
+    thread_send.start()
+
+# Communications
